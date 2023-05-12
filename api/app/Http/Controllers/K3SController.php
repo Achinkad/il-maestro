00b6202e7899c0027a8528a7c71241e78a7bda17
joseareia <jose.apareia@gmail.com>
2023-05-12 15:35:59 +0100

    add: laravel api

---
+++ api/app/Http/Controllers/K3SController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class K3SController extends Controller
+{
+    public function getNamespace()
+    {
+        return "test";
+    }
+}
